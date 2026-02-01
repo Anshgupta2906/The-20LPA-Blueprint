60bb654812e0fa8abf8e5e0353a6b6648d3bb5f6
Anshgupta2906 <anshgupta7785@gmial.com>
2026-02-01 18:31:33 +0530

    i have done two more question

---
+++ 01_Warmup/05_printstr.py
@@ -0,0 +1,20 @@
+# The included code stub will read an integer,n, from STDIN.
+
+# Without using any string methods, try to print the following:
+#123....n
+
+# Note that "..." represents the consecutive values in between.
+
+# Example
+#n=5
+# Print the string . 12345
+
+# Input Format
+# The first line contains an integer .
+
+if __name__ == '__main__':
+    n = int(input())
+    i=1
+    while(i<=n):
+        print(i,end="")
+        i+=1
